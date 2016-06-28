dd1d0577fff38db9b3da52a5aa6c9cbdc7f0dbf1
freddieiboy <freddieiboy@gmail.com>
2016-06-27 22:31:22 -0700

    try get/post

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function(req, res) {
   res.send('hello word!');
 });
 
-app.post('/webhook', function (req, res) {
+app.get('/webhook', function (req, res) {
   var data = req.body;
 
   // Make sure this is a page subscription
