63e1d66f3bcd5798c17701922d02e6db930bb775
freddieiboy <freddieiboy@gmail.com>
2016-06-27 20:00:39 -0700

    add webhooks function

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.get('/', function(req, res) {
   res.send('hello word!');
 });
 
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === <i_am_lord_voldemort_bot>) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');
+  }
+});
+
 app.listen(3000, function() {
   console.log('Server listening on port 3000')
 });
