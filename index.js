b097d46b82b31a9df92e6850317ce248ae697f7c
freddieiboy <freddieiboy@gmail.com>
2016-06-27 22:29:59 -0700

    rb

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.post('/webhook', function (req, res) {
   var data = req.body;
 
   // Make sure this is a page subscription
-  if (data.object == 'page') {
+  if (data.object === 'page') {
     // Iterate over each entry
     // There may be multiple if batched
     data.entry.forEach(function(pageEntry) {
-      var pageID = pageEntry.id;
+      var pageId = pageEntry.id;
       var timeOfEvent = pageEntry.time;
 
       // Iterate over each messaging event
@@ -28,15 +28,13 @@ app.post('/webhook', function (req, res) {
         } else if (messagingEvent.postback) {
           receivedPostback(messagingEvent);
         } else {
-          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+          console.log('Webhook received unknown messagingEvent: ', messagingEvent)
         }
       });
     });
 
     // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know you've
-    // successfully received the callback. Otherwise, the request will time out.
+    // You must send back a 200, within 20 seconds, to let us know you've successfully received the callback. Otherwise, the request will time out
     res.sendStatus(200);
   }
 });
@@ -45,12 +43,11 @@ var PAGE_ACCESS_TOKEN = 'EAADylrNKe0gBAMGSOXlSZBwvTWCuAHOpAFPivzaYTlBqZB6MtdBHre
 
 function receivedMessage(event) {
   var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
+  var recipientId = event.recipient.id;
   var timeOfMessage = event.timestamp;
   var message = event.message;
 
-  console.log("Received message for user %d and page %d at %d with message:",
-    senderID, recipientID, timeOfMessage);
+  console.log("Received message for user %d and page %d at %d with message:", senderID, recipientID, timeOfMessage);
   console.log(JSON.stringify(message));
 
   var messageId = message.mid;
@@ -61,31 +58,27 @@ function receivedMessage(event) {
 
   if (messageText) {
 
-    // If we receive a text message, check to see if it matches any special
-    // keywords and send back the corresponding example. Otherwise, just echo
-    // the text we received.
-    switch (messageText) {
+    /*
+    If we receive a text message, check to see if it matches any special keywords and send back the corresponding example. Otherwise, just echo the text we received.
+    */
+    switch(messageText) {
       case 'image':
         sendImageMessage(senderID);
         break;
-
       case 'button':
         sendButtonMessage(senderID);
         break;
-
       case 'generic':
         sendGenericMessage(senderID);
         break;
-
       case 'receipt':
         sendReceiptMessage(senderID);
         break;
-
       default:
         sendTextMessage(senderID, messageText);
     }
   } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
+    sendTextMessage(senderID, "Message with attachment received.")
   }
 }
 
@@ -95,7 +88,7 @@ function sendTextMessage(recipientId, messageText) {
       id: recipientId
     },
     message: {
-      text: messageText
+      test: messageText
     }
   };
 
@@ -104,21 +97,21 @@ function sendTextMessage(recipientId, messageText) {
 
 function callSendAPI(messageData) {
   request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
+    uri: '',
+    qs: {
+      access_token: PAGE_ACCESS_TOKEN
+    },
     method: 'POST',
     json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
+  }, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
       var recipientId = body.recipient_id;
       var messageId = body.message_id;
 
-      console.log("Successfully sent generic message with id %s to recipient %s",
-        messageId, recipientId);
+      console.log('Successfully sent generic message with id %s to recipient %s', messageId, recipientId);
     } else {
-      console.error("Unable to send message.");
-      console.error(response);
+      console.error('Unable to send message.');
+      console.error(reponse);
       console.error(error);
     }
   });
