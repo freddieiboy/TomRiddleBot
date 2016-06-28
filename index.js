acc6989422aa07ffc8fec29a2f3c75cc80092222
freddieiboy <freddieiboy@gmail.com>
2016-06-27 20:05:48 -0700

    New listen function

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.get('/webhook', function (req, res) {
   }
 });
 
-app.listen(3000, function() {
-  console.log('Server listening on port 3000')
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
