00d6421d00b7cf8f8306ff45b6100676f8c83681
freddieiboy <freddieiboy@gmail.com>
2016-06-27 22:26:10 -0700

    typos

--- index.js
+++ index.js
@@ -6,17 +6,6 @@ app.get('/', function(req, res) {
   res.send('hello word!');
 });
 
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
-  }
-});
-
 app.post('/webhook', function (req, res) {
   var data = req.body;
 
