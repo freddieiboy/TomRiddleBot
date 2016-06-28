6873096f23c5892c52b41826436a7454d9df1a2a
freddieiboy <freddieiboy@gmail.com>
2016-06-27 22:34:41 -0700

    bring back the get

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get('/', function(req, res) {
   res.send('hello word!');
 });
 
+// Node.js Example
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === 'i_am_lord_voldemort_bot') {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');
+  }
+});
+
 app.post('/webhook', function (req, res) {
   var data = req.body;
 
