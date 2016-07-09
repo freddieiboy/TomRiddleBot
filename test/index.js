6644f78e965157fa65d30b5b9282ddaedcb7d790
freddieiboy <freddieiboy@gmail.com>
2016-07-09 16:35:32 -0700

    set up basic tests to pass

--- test/index.js
+++ test/index.js
@@ -3,12 +3,12 @@ import { should, expect, assert } from 'chai';
 import { redButton } from '../src/server.js';
 import { setupDefaultScheduleMsg } from '../src/bot/diary';
 
+//TODO: make sure that bot starts up
 describe('Node Server', () => {
   after(function (done) {
       redButton.close();
       done();
   });
-
   it('should return 200', done => {
     http.get('http://127.0.0.1:5000', res => {
       // assert.equal(200, res.statusCode);
@@ -16,31 +16,59 @@ describe('Node Server', () => {
       done();
     });
   });
-
-  //TODO: receive msg 200
-  //TODO: send msg 200
-  //TODO: send at scheduled times
+  it('should successfully send msgs', () => {
+    //TODO
+  });
+  it('should successfully receive msgs', () => {
+    //TODO
+  });
 });
 
-describe('default sheduled times', function() {
-  //TODO: come back and make this test pass. It's not working currently.
-  // beforeEach(() => {
-    const lines = ['Hey, listen.', 'Ready?', 'Let\'s write.'];
-    const PMtimes = ['01:00PM'];
-    const AMtimes = ['01:00AM'];
-    const ids = [1128889967149164];
-    const currentPMTime = '01:00PM';
-    const currentAMTime = '01:00AM';
-  // })
-
-  it('should send PM sheduled times', () => {
-    const schedule = setupDefaultScheduleMsg(PMtimes, ids, lines, currentPMTime);
-    // expect(schedule).to.equal(true);
+//TODO: refractor schedule for one time a day. Make tests pass.
+describe('bot schedule prompt', () => {
+  it('should queue 1 prompt that day', () => {
+    //TODO
   });
-
-  it('should send AM sheduled times', () => {
-    let schedule = setupDefaultScheduleMsg(PMtimes, ids, lines, currentAMTime);
-    console.log(schedule)
-    // expect(schedule).to.equal(true);
+  it('should not have the same prompt as the previous two days', () => {
+    //TODO
   });
+  it('should send at scheduled time', () => {
+    //TODO
+  });
+  it('should allow users to change sheduled time', () => {
+    //TODO
+  });
+  it('should return to default time on command', () => {
+
+  });
+});
+
+//TODO: refactor bot lines for longer answers. Make tests pass.
+describe('bot text response', () => {
+  it('should wait a few seconds between each text before sending out a thank you response', () => {
+    //TODO
+  });
+  it('should send a thank you response', () => {
+    //TODO
+  });
+});
+
+//TODO: save journal entry for users when they sign in.
+describe('saving journal entries', () => {
+  it('should save a journal entry when a user sends an entry', () => {
+    //TODO
+  });
+  it('should batch journal entries by date.', () => {
+    //TODO
+  });
+  it('should save users ID, createdTime, and createdDate', () => {
+    //TODO
+  })
+});
+
+//TODO: add redux for state management.
+describe('state tree', () => {
+  it('should hydrate some content on redux every few hours', () => {
+    //TODO
+  })
 });
